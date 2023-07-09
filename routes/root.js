c9d778d0888415284fe64a6651a707f45cc8a735
JUAN JOSE LANGA MATA <juanjoselangamata@JUANs-MBP.home>
2023-07-09 19:41:39 +0200

    Adding middleware

--- routes/root.js
+++ routes/root.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
-const path = require('path')
+const router = express.Router();
+const path = require('path');
 
 router.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
-})
+	res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+});
 
-module.exports = router
+module.exports = router;
